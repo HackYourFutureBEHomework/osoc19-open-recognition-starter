eb37b422ee8ff8fa3c2fb4ee0b929e417f83e324
ZHanim <zilhan.be@gmail.com>
2019-07-09 11:13:25 +0200

    installing axios

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import logo from '../images/oR-logo.png';
+import searchbar from '../images/searchbar.png';
 
 export class Header extends React.Component {
     // constructor() {
@@ -14,6 +15,7 @@ export class Header extends React.Component {
         return (
             <div className="Header">
                 <img src={logo} alt="Open Recognition Logo" className="logo"/>
+                <img src={searchbar} alt="Search Bar" className="Searchbar"/>
                 <a href=" " className="Signout">Sign Out</a>
             </div>
         );
--- client/src/components/StatementList.jsx
+++ client/src/components/StatementList.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { StatementItem } from "./StatementItem";
+import axios from "axios";
 
 export class StatementList extends React.Component {
-    // constructor() {
-    //     super();
-        
-    // }
+    constructor() {
+        super();
+        this.state = {
+            statements: []
+        };
+    }
 
     render() {
         return (
