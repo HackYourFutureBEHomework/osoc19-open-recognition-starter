b21d4d861fe423a940c276eb814e7967288a2efd
itsmegith <bettercallgita@gmail.com>
2019-07-19 23:44:47 +0200

    edited data model

--- api-server/connection/model.js
+++ api-server/connection/model.js
@@ -4,16 +4,11 @@ const mongoose = require('mongoose');
 const connection_schema = mongoose.Schema({
   requester: { type: mongoose.Schema.Types.ObjectId, ref: 'Users'},
   recipient: { type: mongoose.Schema.Types.ObjectId, ref: 'Users'},
-  status: {
-    type: Number,
-    enums: [
-        0,    //'add friend',
-        1,    //'requested',
-        2,    //'pending',
-        3,    //'friends'
-    ]
-  }
-}, {timestamps: true})
+  accepted: {
+    type: Boolean,
+    default: false
+   }
+})
 
 
 
