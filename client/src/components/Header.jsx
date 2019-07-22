1a0fa7610b18f96a51c2fb20a2ddf2c53b7dc911
ZHanim <zilhan.be@gmail.com>
2019-07-22 22:08:31 +0200

    directory change

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,18 +1,38 @@
 import React, { Component } from "react";
-import logo from "../images/orson.png";
+import logo from "../img/logo-50x.png";
 
 class Header extends Component {
   state = {};
   render() {
     return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-light shadow">
-        <div className="container">
-          <img src={logo} alt="Open Recognition Logo" className="logo_on" />
-              <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                  <span className="navbar-toggler-icon"></span>
-              </button>
+      <header>
+         <nav class="navbar navbar-expand-lg navbar-dark fixed-top scrolling-navbar aqua-gradient">
+          <div class="container">
+            <a class="navbar-brand" href="#">
+              <img src={logo} alt="Open Recognition Logo" className="logo_on" />
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-7" aria-controls="navbarSupportedContent-7"
+            aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent-7">
+              <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                <a class="nav-link" href="#"></a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#"></a>
+                </li>
+              </ul>
+            <form class="form-inline">
+              <div class="md-form my-0">
+                <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" />
+              </div>
+            </form>
+          </div>
         </div>
       </nav>
+  </header>
     );
   }
 }
